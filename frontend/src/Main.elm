e320636deb56473c60d57b63fe1ca1c75f7ac0ab
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2021-06-09 18:07:36 -0400

    switch to using Browser.application in frontend example

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -7,9 +7,11 @@ module Main exposing (..)
 --
 
 
-import Browser
+import Browser exposing (UrlRequest)
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Url exposing (Url)
+import Browser.Navigation as Nav
 
 
 
@@ -17,7 +19,13 @@ import Html.Events exposing (onClick)
 
 
 main =
-  Browser.document { init = init, update = update, view = view, subscriptions = subscriptions }
+  Browser.application { init = init
+                      , update = update
+                      , view = view
+                      , subscriptions = subscriptions
+                      , onUrlRequest = onUrlRequest
+                      , onUrlChange = onUrlChange
+                      }
 
 
 
@@ -27,8 +35,8 @@ main =
 type alias Model = Int
 
 
-init : () -> (Model, Cmd Msg)
-init _ =
+init : () -> Url -> Nav.Key -> (Model, Cmd Msg)
+init _ _ _ =
   (0, Cmd.none)
 
 
@@ -36,6 +44,15 @@ init _ =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ = Sub.none
+
+
+-- Url stuff
+
+onUrlRequest : UrlRequest -> Msg
+onUrlRequest _ = Increment
+
+onUrlChange : Url -> Msg
+onUrlChange _ = Increment
 
 
 -- UPDATE
