ebbb437b08c42686f89bb105f989ee527669cc35
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2021-06-09 20:11:17 -0400

    experiment with some css

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -10,6 +10,7 @@ module Main exposing (..)
 import Browser exposing (UrlRequest)
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (style, href)
 import Url exposing (Url)
 import Browser.Navigation as Nav
 
@@ -91,14 +92,33 @@ view model =
   { title = "thitle"
   , body = 
       [
-        div []
+        div centralStyle
+        [ div []
           [ button [ onClick Decrement ] [ text "-" ]
           , div [] [ text (String.fromInt model.numState) ]
           , button [ onClick Increment ] [ text "+" ]
           ]
-      , div []
+        , div []
           [
             text model.url.path
           ]
+        , div []
+          [ Html.a [href "https://www.example.com"] [ text "a link" ] ]
+        ]
       ]
   }
+
+-- Styles
+
+centralStyle : List (Html.Attribute msg)
+centralStyle =
+  [ style "color" "#222"
+  , style "background-color" "#888"
+  , style "font-size" "18pt"
+  , style "font-family" "arial"
+  , style "text-align" "center"
+  , style "padding-left" "10%"
+  , style "padding-right" "10%"
+  , style "padding-top" "40vh"
+  , style "height" "60vh"
+  ]
